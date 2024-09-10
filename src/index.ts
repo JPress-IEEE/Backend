01ee7d2ea8930059ad32bf3fbd93dcd4e1ad5714
Basma-90 <basmasabry33333@gmail.com>
2024-09-10 18:15:26 +0300

    feat(server): add server configuration and routes

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import expressRateLimit from 'express-rate-limit';
+import { dbConnect ,dbDisconnect} from './DBConfig/db.config';
+import userRouter from './routes/user.route';
+import passport from './utils/passport.utils';
+import session from 'express-session';
+
+const app = express();
+dbConnect();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(expressRateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/auth', userRouter);   
+
+const server = app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+export default server;
