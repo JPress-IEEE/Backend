974b6281634e04afa53070b2a0fad6f411d85bb3
Basma-90 <basmasabry33333@gmail.com>
2024-09-10 18:15:17 +0300

    feat(config): add custom-environment-variables.ts and default.ts for environment variable configuration

---
+++ config/custom-environment-variables.ts
@@ -0,0 +1,11 @@
+export default{
+    privateKey: 'PRIVATE_KEY',
+    publicKey: 'PUBLIC_KEY',
+    port: 'PORT',
+    mongoUrl: 'MONGO_URI',
+    accessTokenTTL: 'ACCESS_KEY_TTL',
+    refreshTokenTTL: 'REFRESH_KEY_TTL',
+    salt: 'SALT_WORK_FACTOR',
+    nodeEnv: 'NODE_ENV',
+    sessionSecret: 'SESSION_SECRET',
+}
---
+++ config/default.ts
@@ -0,0 +1,13 @@
+import { session } from "passport";
+
+export default{
+    privateKey: process.env.PRIVATE_KEY || '',
+    publicKey: process.env.PUBLIC_KEY || '',
+    port: process.env.PORT || '',
+    mongoUrl: process.env.MONGO_URI || '',
+    accessTokenTTL: process.env.ACCESS_KEY_TTL || "15m",
+    refreshTokenTTL: process.env.REFRESH_KEY_TTL || "1y",
+    salt: process.env.SALT_WORK_FACTOR || 10,
+    nodeEnv: process.env.NODE_ENV ,
+    sessionSecret: process.env.SESSION_SECRET ||,
+}
