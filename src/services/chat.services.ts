b0ce07a10e00941bf0ed3b9a1199f6817e343835
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-11 21:55:14 +0300

    feat(chat): add chat service

---
+++ src/services/chat.services.ts
@@ -0,0 +1,5 @@
+const createChat = async (participant1_id: string, participant2_id: string): Promise<void> => {};
+
+const getChatById = async (chatId: string): Promise<void> => {};
+
+export { createChat, getChatById };
