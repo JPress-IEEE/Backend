8a9a032713efbada8cf07275c582e3d3aca43bab
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-11 19:43:42 +0300

    feat(chat): add chat routes

---
+++ src/routes/chat.route.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createChat } from "../controllers/chat.controller";
+import { getChatMessages } from "../controllers/chat.controller";
+
+const router = Router();
+
+router.post("/chat", createChat);
+router.post("/chat/:chatId/messages", getChatMessages);
+
+export default router;
