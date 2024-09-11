65f434e04a605a03b8eb6138543d05e4e18de6fb
Basma-90 <basmasabry33333@gmail.com>
2024-09-11 05:31:08 +0300

    feat: Add client controller, schema, and services

---
+++ src/controllers/client.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import * as clientServices from '../services/client.services';
+import { IClient } from '../models/client.model';
+import { ClientSchema } from '../schemas/client.schema';
+
+/*
+
+export const createClient = async (req: Request, res: Response): Promise<void> => {
+};
+
+export const getClientById = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const getClientByEmail = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const getClients = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const updateClient = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const deleteClient = async (req: Request, res: Response): Promise<void> => {
+    
+}
+
+export const deleteClientByEmail = async (req: Request, res: Response): Promise<void> => {
+    
+}
+*/
+
+
---
+++ src/services/client.services.ts
@@ -0,0 +1,31 @@
+import Client,{IClient} from "../models/client.model";
+import { ClientSchema } from "../schemas/client.schema";
+
+/*
+export const createClient = async (client: IClient): Promise<IClient> => {
+};
+
+export const getClientById = async (clientId: string): Promise<IClient | null> => {
+    
+}
+
+export const getClientByEmail = async (email: string): Promise<IClient | null> => {
+    
+}
+
+export const getClients = async (): Promise<IClient[]> => {
+    
+}
+
+export const updateClient = async (clientId: string, client: IClient): Promise<IClient | null> => {
+    
+}
+
+export const deleteClient = async (clientId: string): Promise<IClient | null> => {
+    
+}
+
+export const deleteClientByEmail = async (email: string): Promise<IClient | null> => {
+    
+}
+*/
