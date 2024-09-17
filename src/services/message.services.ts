e6286e451f6dd39888eb387de078684dff4d7f5f
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-17 19:45:35 +0300

    refactor(message): add message serivces logic

--- src/services/message.services.ts
+++ src/services/message.services.ts
@@ -1,11 +1,42 @@
-const createMessage = async (chatId: string, senderId: string, content: string): Promise<void> => {};
+import Message from "../models/message.model";
+import { messageSchema } from "../schemas/message.schema";
 
-const getMessageForChat = async (chatId: string): Promise<void> => {};
+const createMessage = async (chatId: string, senderId: string, content: string) => {
+  const validationMessage = messageSchema.safeParse({ chat_id: chatId, sender_id: senderId, content });
+  if (!validationMessage.success) throw new Error(validationMessage.error.message);
 
-const editMessage = async (messageId: string, newContent: string): Promise<void> => {};
+  const message = new Message({
+    chat_id: chatId,
+    sender_id: senderId,
+    content,
+  });
 
-const deleteMessage = async (messageId: string): Promise<void> => {};
+  await message.save();
+  return message;
+};
 
-const markMessageAsRead = async (messageId: string): Promise<void> => {};
+const getMessageForChat = async (chatId: string) => {
+  const messages = await Message.find({ chat_id: chatId }).populate("sender_id", "name").sort({ createdAt: "asc" });
+  return messages;
+};
+
+const editMessage = async (messageId: string, newContent: string) => {
+  const message = await Message.findByIdAndUpdate(messageId, { content: newContent }, { new: true });
+  if (!message) throw new Error("Message not found");
+  return message;
+};
+
+const deleteMessage = async (messageId: string) => {
+  const message = await Message.findByIdAndDelete(messageId);
+  if (!message) throw new Error("Message not found");
+
+  return message;
+};
+
+const markMessageAsRead = async (messageId: string) => {
+  const message = await Message.findByIdAndUpdate(messageId, { isRead: true }, { new: true });
+  if (!message) throw new Error("Message not found");
+  return message;
+};
 
 export { createMessage, getMessageForChat, editMessage, deleteMessage, markMessageAsRead };
