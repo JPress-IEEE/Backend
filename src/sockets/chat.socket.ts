1622255f66f0731ee62ab09ad6fc4e6bd7ad6b01
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-17 22:40:20 +0300

    refactor(chat): add chat handling with socket.io

--- src/sockets/chat.socket.ts
+++ src/sockets/chat.socket.ts
@@ -1,5 +1,27 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
+import * as messageService from "../services/message.services";
 
-const handleChatSockets = (io: Server) => {};
+const handleChatSockets = (io: Server) => {
+  io.on("connection", (socket: Socket) => {
+    console.log("User connected: ", socket.id);
+
+    socket.on("joinChat", (chatId: string) => {
+      socket.join(chatId);
+    });
+
+    socket.on("sendMessage", async (chatId: string, senderId: string, content: string) => {
+      try {
+        const message = await messageService.createMessage(chatId, senderId, content);
+        io.to(chatId).emit("receiveMessage", message);
+      } catch (error: any) {
+        socket.emit("errorMessage", error.message);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected", socket.id);
+    });
+  });
+};
 
 export { handleChatSockets };
