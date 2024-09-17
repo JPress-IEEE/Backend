6bfaf0fa2245d959fde2929d81e174eb04b1d9ca
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-17 19:44:20 +0300

    refactor(chat): add chat serivces logic

--- src/services/chat.services.ts
+++ src/services/chat.services.ts
@@ -1,5 +1,31 @@
-const createChat = async (participant1_id: string, participant2_id: string): Promise<void> => {};
+import Chat from "../models/chat.model";
+import { chatSchema } from "../schemas/chat.schema";
 
-const getChatById = async (chatId: string): Promise<void> => {};
+const createChat = async (participant1_id: string, participant2_id: string) => {
+  const validationChat = chatSchema.safeParse({ participant1_id, participant2_id });
+  if (!validationChat.success) throw new Error(validationChat.error.message);
+
+  const existingChat = await Chat.findOne({
+    participant1_id,
+    participant2_id,
+  });
+
+  if (existingChat) {
+    return existingChat;
+  }
+  const newChat = new Chat({
+    participant1_id,
+    participant2_id,
+  });
+
+  await newChat.save();
+  return newChat;
+};
+
+const getChatById = async (chatId: string) => {
+  const chat = await Chat.findById(chatId).populate("participant1_id").populate("participant2_id");
+  if (!chat) throw new Error("Chat not found");
+  return chat;
+};
 
 export { createChat, getChatById };
