e5fa7da51aed12612c2e6392fe4804a0e8341686
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-17 22:26:34 +0300

    refactor(notification): add controllers logic for notification management

--- src/controllers/notification.controller.ts
+++ src/controllers/notification.controller.ts
@@ -1,11 +1,40 @@
-import * as notificationService from '../services/notification.services';
-import { Request, Response } from 'express';
-import { INotification } from '../models/notification.model';
-import { validateNotificationCreate, validateNotificationDelete, validateNotificationGet, validateNotificationUpdate } from '../services/notification.services';
+import { Request, Response } from "express";
+import * as notificationService from "../services/notification.services";
 
-export const createNotification = async (req: Request, res: Response) => {};
-export const getNotification = async (req: Request, res: Response) => {};
-export const updateNotification = async (req: Request, res: Response) => {};
-export const deleteNotification = async (req: Request, res: Response) => {};
-
-
+export const createNotification = async (req: Request, res: Response) => {
+  try {
+    const { userId, message, status } = req.body;
+    const notification = await notificationService.createNotification(userId, message, status);
+    res.status(201).json(notification);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
+export const getNotification = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const notification = await notificationService.getNotification(userId);
+    res.status(200).json(notification);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const updateNotification = async (req: Request, res: Response) => {
+  try {
+    const { notificationId } = req.params;
+    const { status } = req.body;
+    const notification = await notificationService.updateNotification(notificationId, status);
+    res.status(200).json(notification);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
+export const deleteNotification = async (req: Request, res: Response) => {
+  try {
+    const { notificationId } = req.params;
+    await notificationService.deleteNotification(notificationId);
+    res.status(204).send();
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
