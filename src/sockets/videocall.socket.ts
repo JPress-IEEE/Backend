535f454d65aa0f605a5b1770b018f040b9c5da25
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-17 22:41:02 +0300

    refactor(videoCall): add video call handling with socket.io

--- src/sockets/videocall.socket.ts
+++ src/sockets/videocall.socket.ts
@@ -1,5 +1,18 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
+import * as videocallService from "../services/videocall.services";
 
-const handleVideoCallSockets = (io: Server) => {};
+const handleVideoCallSockets = (io: Server) => {
+  io.on("connection", (socket: Socket) => {
+    socket.on("startCall", async (chatId: string) => {
+      const call = await videocallService.startVideoCall(chatId);
+      io.to(chatId).emit("callStarted: ", call);
+    });
+
+    socket.on("endCall", async (chatId: string) => {
+      const call = await videocallService.endVideoCall(chatId);
+      io.to(chatId).emit("callEnded", call);
+    });
+  });
+};
 
 export { handleVideoCallSockets };
