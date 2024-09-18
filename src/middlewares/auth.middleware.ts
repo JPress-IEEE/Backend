7265b3334a173328db5f92578b4830bc0b5a16c9
Basma-90 <basmasabry33333@gmail.com>
2024-09-19 00:50:21 +0300

    refactor: Add admin role middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -76,3 +76,29 @@ export const applicantRoleMiddleware = async (req: Request, res: Response, next:
     }
 };
 
+export const adminRoleMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const accessToken = req.headers.authorization?.split(' ')[1];
+        if (!accessToken) {
+            return res.status(401).send({ message: 'Access token not found' });
+        }
+        const decoded = verifyToken(accessToken) as {
+            [x: string]: any; userId: string
+        };
+        const userId: string = decoded.user.id as string;
+        const user = await getUserById(userId);
+        if (!user) {
+            return res.status(401).send({ message: 'User not found' });
+        }
+        if (user.role !== 'admin') {
+            return res.status(403).send({ message: 'Unauthorized' });
+        }
+        req.body.userId = userId;
+        req.body.role = user.role;
+        next();
+    }
+    catch (err) {
+        next(err);
+    }
+};
+
