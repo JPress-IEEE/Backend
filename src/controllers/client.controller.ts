1ea24da73cfe3225046b3ff8ba19594d574319ee
Basma-90 <basmasabry33333@gmail.com>
2024-09-19 06:09:52 +0300

    refactor: Add client controller functions and routes
    - Added createClient, getClientById, getClients, updateClient, deleteClient, and uploadImage functions to the client controller.
    - Updated client route to include authentication and authorization middleware.
    - Added profile picture upload functionality to the client controller.
    - Updated client services to include CRUD operations for clients and uploading client images.

--- src/controllers/client.controller.ts
+++ src/controllers/client.controller.ts
@@ -3,34 +3,92 @@ import * as clientServices from '../services/client.services';
 import { IClient } from '../models/client.model';
 import { ClientSchema } from '../schemas/client.schema';
 
-/*
+
 
 export const createClient = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const client: IClient = req.body;
+        const validatedClient = await ClientSchema.parseAsync(client) as IClient;
+        const result = await clientServices.createClient(validatedClient);
+        res.status(201).json(result);
+    } catch (err: any) {
+        res.status(400).json({ message: err.message });
+    }
 };
 
 export const getClientById = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const id = req.body.userId;
+        const clientId = req.params.id;
+        if (clientId !== id) {
+            res.status(401).json({ message: 'Unauthorized' });
+            return;
+        }
+        const result = await clientServices.getClientById(clientId);
+        res.status(200).json(result);
+    } catch (err: any) {
+        res.status(400).json({ message: err.message });
+    }
     
-}
-
-export const getClientByEmail = async (req: Request, res: Response): Promise<void> => {
-    
-}
+};
 
 export const getClients = async (req: Request, res: Response): Promise<void> => {
-    
-}
+    try {
+        const queryString = req.query as unknown as string;
+        const result = await clientServices.getClients(queryString);
+        res.status(200).json(result);
+    } catch (err: any) {
+        res.status(400).json({ message: err.message });
+    }  
+};
 
 export const updateClient = async (req: Request, res: Response): Promise<void> => {
-    
-}
+    try {
+        const id = req.body.userId;
+        const clientId = req.params.id;
+        if(clientId !== id){
+            res.status(401).json({ message: 'Unauthorized' });
+            return;
+        }
+        const client: IClient = req.body;
+        const validatedClient = await ClientSchema.parseAsync(client) as IClient;
+        const result = await clientServices.updateClient(clientId, validatedClient);
+        res.status(200).json(result);
+    } catch (err: any) {
+        res.status(400).json({ message: err.message });
+    }
+};
 
 export const deleteClient = async (req: Request, res: Response): Promise<void> => {
-    
-}
+    try {
+        const id = req.body.userId;
+        const clientId = req.params.id;
+        if(clientId !== id){
+            res.status(401).json({ message: 'Unauthorized' });
+            return;
+        }   
+        const result = await clientServices.deleteClient(clientId);
+        res.status(200).json(result);
+    } catch (err: any) {
+        res.status(400).json({ message: err.message });
+    }
+};
 
-export const deleteClientByEmail = async (req: Request, res: Response): Promise<void> => {
-    
-}
-*/
+export  const uploadImage = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const id = req.body.userId;
+        const clientId = req.params.id;
+        if(clientId !== id){
+            res.status(401).json({ message: 'Unauthorized' });
+            return;
+        }
+        const client: IClient = req.body;
+        const result = await clientServices.uploadClientImage(clientId, clientId);
+        res.status(200).json(result);
+    } catch (err: any) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
 
 
--- src/routes/client.route.ts
+++ src/routes/client.route.ts
@@ -1,14 +1,15 @@
 import * as clientController from '../controllers/client.controller';
 import { Router } from 'express';
+import { adminRoleMiddleware, authMiddleware,clientRoleoleMiddleware } from '../middlewares/auth.middleware';
+import   {profilePicUpload } from '../utils/upload.utils';
+import { uploadImage } from '../controllers/client.controller';
 
-/*
 const clientRouter = Router();
 
-clientRouter.get('/', clientController.getClients);
-clientRouter.get('/:id', clientController.getClient);
-clientRouter.post('/', clientController.createClient);
-clientRouter.put('/:id', clientController.updateClient);
-clientRouter.delete('/:id', clientController.deleteClient);
-
+clientRouter.post('/', authMiddleware, clientRoleoleMiddleware, clientController.createClient);
+clientRouter.get('/:id', authMiddleware,clientRoleoleMiddleware, clientController.getClientById);
+clientRouter.put('/:id', authMiddleware, clientRoleoleMiddleware, clientController.updateClient);
+clientRouter.delete('/:id', authMiddleware, clientRoleoleMiddleware, clientController.deleteClient);
+clientRouter.get('/', authMiddleware,adminRoleMiddleware, clientController.getClients);
+clientRouter.post('/:id/profile-pic', authMiddleware, clientRoleoleMiddleware, profilePicUpload.single('profilePic'),uploadImage);
 export default clientRouter;
-*/
