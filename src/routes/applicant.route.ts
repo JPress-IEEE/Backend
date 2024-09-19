eb256c2352dee73b628d6104b33a72b1fcb9ac11
Basma-90 <basmasabry33333@gmail.com>
2024-09-19 06:11:21 +0300

    Refactor : update applicant route ,service file and controller

--- src/routes/applicant.route.ts
+++ src/routes/applicant.route.ts
@@ -1,14 +1,20 @@
 import * as applicantController from '../controllers/applicant.controller';
 import { Router } from 'express';
+import   { resumeUpload, profilePicUpload } from '../utils/upload.utils';
+import { authMiddleware, applicantRoleMiddleware ,adminRoleMiddleware} from '../middlewares/auth.middleware';
+import { getApplicants,getApplicant,createApplicant,updateApplicant,deleteApplicant,uploadResume,uploadProfilePic } from '../controllers/applicant.controller';
 
-/*
+
+
 const applicantRouter = Router();
 
-applicantRouter.get('/', applicantController.getApplicants);
-applicantRouter.get('/:id', applicantController.getApplicant);
-applicantRouter.post('/', applicantController.createApplicant);
-applicantRouter.put('/:id', applicantController.updateApplicant);
-applicantRouter.delete('/:id', applicantController.deleteApplicant);
-
+applicantRouter.get('/',authMiddleware,adminRoleMiddleware,getApplicants);
+applicantRouter.get('/:id',authMiddleware,applicantRoleMiddleware, getApplicant);
+applicantRouter.post('/',authMiddleware,applicantRoleMiddleware,createApplicant);
+applicantRouter.put('/:id',authMiddleware,applicantRoleMiddleware, updateApplicant);
+applicantRouter.delete('/:id',authMiddleware,applicantRoleMiddleware,deleteApplicant);
+applicantRouter.put('/:id/resume',authMiddleware,applicantRoleMiddleware,resumeUpload.single('resume'),uploadResume);
+applicantRouter.put('/:id/profilePic',authMiddleware,applicantRoleMiddleware,profilePicUpload.single('profilePic'),uploadProfilePic);
 export default applicantRouter;
-*/
+
+
