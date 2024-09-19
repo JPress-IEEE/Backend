d424c8ecadd20b840a97841b504298751043a887
Basma-90 <basmasabry33333@gmail.com>
2024-09-19 22:52:12 +0300

    refactor: Update express middleware configuration

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,8 @@ dbConnect();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
     secret: process.env.SESSION_SECRET!,
