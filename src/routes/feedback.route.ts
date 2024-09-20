3a00856e7ae0c7f0ce5761e95463fd1e259ee0dc
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-20 12:26:04 +0300

    feat(feedback) add feedback routes

--- src/routes/feedback.route.ts
+++ src/routes/feedback.route.ts
@@ -1,14 +1,13 @@
 import * as feedbackController from '../controllers/feedback.controller';
 import { Router } from 'express';
 
-/*
+
 const feedbackRouter = Router();
 
 feedbackRouter.get('/', feedbackController.getFeedbacks);
-feedbackRouter.get('/:id', feedbackController.getFeedback);
+feedbackRouter.get('/:id', feedbackController.getFeedbackById);
 feedbackRouter.post('/', feedbackController.createFeedback);
 feedbackRouter.put('/:id', feedbackController.updateFeedback);
 feedbackRouter.delete('/:id', feedbackController.deleteFeedback);
 
 export default feedbackRouter;
-*/
