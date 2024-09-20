f57c7bc2118b6b40c67cc375b31c2e35dc2ffed0
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-20 12:30:39 +0300

    feat(feedback) add feedback services and controllers (create - get - getById - update - delete)

--- src/services/feedback.services.ts
+++ src/services/feedback.services.ts
@@ -1,41 +1,48 @@
-import Feedback ,{IFeedback} from "../models/feedback.model";
+import Feedback, { IFeedback } from "../models/feedback.model";
 import { FeedbackSchema } from "../schemas/feedback.schema";
 
-/*
-export const createFeedback = async (feedback: IFeedback): Promise<IFeedback> => {
-   
+export const createFeedback = async (applicantId: string, clientId: string, rate: number, comment: string) => {
+  const validationFeedback = FeedbackSchema.safeParse({
+    applicantId,
+    clientId,
+    rate,
+    comment,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+
+  if (!validationFeedback.success) throw new Error(validationFeedback.error.message);
+
+  const feedback = new Feedback({
+    applicantId,
+    clientId,
+    rate,
+    comment,
+  });
+
+  await feedback.save();
+  return feedback;
 };
 
-export const getFeedbackById = async (feedbackId: string): Promise<IFeedback | null> => {
-    
+export const getFeedbackById = async (feedbackId: string) => {
+  const feedback = await Feedback.findById(feedbackId);
+  if (!feedback) throw new Error("Feedback not found");
+  return feedback;
 };
 
-export const getFeedbackByUserId = async (userId: string): Promise<IFeedback | null> => {
-    
+export const getFeedbacks = async () => {
+  const feedbacks = await Feedback.find().sort({ createdAt: "desc" });
+  return feedbacks;
 };
 
-export const getFeedbackByOfferId = async (offerId: string): Promise<IFeedback | null> => {
-    
+export const updateFeedback = async (feedbackId: string, rate: number, comment: string) => {
+  const feedback = await Feedback.findByIdAndUpdate(feedbackId, { rate, comment, updatedAt: new Date() }, { new: true });
+  if (!feedback) throw new Error("Feedback not found");
+  return feedback;
 };
 
-export const getFeedbacks = async (): Promise<IFeedback[]> => {
-    
+export const deleteFeedback = async (feedbackId: string) => {
+  const feedback = await Feedback.findByIdAndDelete(feedbackId);
+  if (!feedback) throw new Error("Feedback not found");
+  return feedback;
 };
-
-export const updateFeedback = async (feedbackId: string, feedback: IFeedback): Promise<IFeedback | null> => {
-    
-};
-
-export const deleteFeedback = async (feedbackId: string): Promise<IFeedback | null> => {
-    
-};
-
-export const deleteFeedbackByUserId = async (userId: string): Promise<IFeedback | null> => {
-    
-};
-
-export const deleteFeedbackByOfferId = async (offerId: string): Promise<IFeedback | null> => {
-    
-};
-
-*/
