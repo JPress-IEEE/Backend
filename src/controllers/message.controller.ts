6e3662febda690cc95c9aa2427ca6772bae29778
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-22 00:02:09 +0300

    fix(message): fix error handling

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import * as messageService from "../services/message.services";
+import { messageSchema } from "../schemas/message.schema";
 
 const createMessage = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { chatId, senderId, content } = req.body;
+
+    const validationMessage = messageSchema.safeParse({ chat_id: chatId, sender_id: senderId, content });
+    if (!validationMessage.success) return res.status(400).json({ message: validationMessage.error.issues[0].message });
+
     const message = await messageService.createMessage(chatId, senderId, content);
     res.status(201).json(message);
   } catch (error: any) {
@@ -28,6 +33,7 @@ const editMessage = async (req: Request, res: Response, next: NextFunction) => {
     const { messageId } = req.params;
     const { newContent } = req.body;
     const message = await messageService.editMessage(messageId, newContent);
+    if (!message) return res.status(404).json({ message: "Message not found" });
     res.status(200).json(message);
   } catch (error: any) {
     // res.status(400).json({ message: error.message });
@@ -38,7 +44,8 @@ const editMessage = async (req: Request, res: Response, next: NextFunction) => {
 const deleteMessage = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { messageId } = req.params;
-    await messageService.deleteMessage(messageId);
+    const message = await messageService.deleteMessage(messageId);
+    if (!message) return res.status(404).json({ message: "Message not found" });
     res.status(204).json({ message: `${messageId} message is removed` });
   } catch (error: any) {
     // res.status(400).json({ message: error.message });
@@ -50,6 +57,7 @@ const markMessageAsRead = async (req: Request, res: Response, next: NextFunction
   try {
     const { messageId } = req.params;
     const message = await messageService.markMessageAsRead(messageId);
+    if (!message) return res.status(404).json({ message: "Message not found" });
     res.status(200).json(message);
   } catch (error: any) {
     // res.status(400).json({ message: error.message });
