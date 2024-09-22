009d56f3ba52f78489faa83b6f4677a6033abab8
Basma-90 <basmasabry33333@gmail.com>
2024-09-22 16:49:48 +0300

    feat : add bookmark controller, services and routes

--- src/controllers/bookmark.controller.ts
+++ src/controllers/bookmark.controller.ts
@@ -1,41 +1,86 @@
-import { Request, Response } from 'express';
-import * as bookmarkServices from '../services/client.services';
+import e, { Request, Response ,NextFunction} from 'express';
+import * as bookmarkServices from '../services/bookmark.services';
 import { IBookmark } from '../models/bookmark.model';
 import {BookmarkSchema} from '../schemas/bookmark.schema';
+import { ZodError } from 'zod';
 
-/*
-export const createBookmark = async (req: Request, res: Response): Promise<void> => {
+export const createBookmark = async (req: Request, res: Response ,next:NextFunction): Promise<void> => {
+    try {
+        const bookmark= BookmarkSchema.parse(req.body) as unknown as IBookmark;
+        if(bookmark.userId!==req.body.userId){
+                res.status(401).send('Unauthorized');
+                return;
+        }
+        const newBookmark = await bookmarkServices.createBookmark(bookmark);
+        res.status(201).json(newBookmark);
+    } catch (error:any) {
+        if (error instanceof ZodError) {
+            res.status(400).send(error.errors);
+        }else{
+       next(error);
+        }
+    }
 };
 
-export const getBookmarkById = async (req: Request, res: Response): Promise<void> => {
-    
+export const getBookmarkById = async (req: Request, res: Response,next:NextFunction): Promise<void> => {
+    try {
+        const bookmarkId = req.params.id;
+        const bookmark = await bookmarkServices.getBookmarkById(bookmarkId);
+        if(!bookmark){
+            res.status(404).send('Bookmark not found');
+            return;
+        }
+        if(bookmark?.userId?.toString()!==req.body.userId){
+                res.status(401).send('Unauthorized');
+                return;
+        }
+        if (bookmark) {
+            res.status(200).json(bookmark);
+        } else {
+            res.status(404).send('Bookmark not found');
+        }
+    } catch (error:any) {
+        next(error);
+    }
 }
 
-export const getBookmarkByUserId = async (req: Request, res: Response): Promise<void> => {
-    
+export const getBookmarksByUserId = async (req: Request, res: Response,next:NextFunction): Promise<void> => {
+    try {
+        const userId = req.params.id;
+        if(userId!==req.body.userId){
+                res.status(401).send('Unauthorized');
+                return;
+        }
+        const bookmarks = await bookmarkServices.getBookmarksByUserId(userId);
+        if (bookmarks) {
+            res.status(200).json(bookmarks);
+        } else {
+            res.status(404).send('Bookmarks not found');
+        }
+    } catch (error:any) {
+        next(error);
+    }
 }
 
-export const getBookmarkByOfferId = async (req: Request, res: Response): Promise<void> => {
-    
+export const deleteBookmark = async (req: Request, res: Response,next:NextFunction): Promise<void> => {
+    try {
+        const bookmarkId = req.params.id;
+        const bookmark = await bookmarkServices.deleteBookmark(bookmarkId);
+        if(!bookmark){
+            res.status(404).send('Bookmark not found');
+            return;
+        }
+        if(bookmark?.userId?.toString()!==req.body.userId){
+                res.status(401).send('Unauthorized');
+                return;
+        }
+        if (bookmark) {
+            res.status(200).json(bookmark);
+        } else {
+            res.status(404).send('Bookmark not found');
+        }
+    } catch (error:any) {
+        next(error);
+    }
 }
 
-export const getBookmarks = async (req: Request, res: Response): Promise<void> => {
-    
-}
-
-export const updateBookmark = async (req: Request, res: Response): Promise<void> => {
-    
-}
-
-export const deleteBookmark = async (req: Request, res: Response): Promise<void> => {
-    
-}
-
-export const deleteBookmarkByUserId = async (req: Request, res: Response): Promise<void> => {
-    
-}
-
-export const deleteBookmarkByOfferId = async (req: Request, res: Response): Promise<void> => {
-    
-}
-*/
